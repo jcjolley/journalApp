26c15c8033087557b391e8a39a75db883918f2c1
Gage <justgage@gmail.com>
2014-05-16 07:02:35 -0600

    fixed indentation to match rest of the code

--- Markdown.java
+++ Markdown.java
@@ -1,13 +1,15 @@
+import java.util.regex.*;
+
 public class Markdown {
 
-   public static int parseHeader(String line) {
-      int i = 0;
-      while(line.charAt(i) == '#') {
-         i++;
-      }
+ public static int parseHeader(String line) {
+  int i = 0;
+  while(line.charAt(i) == '#') {
+   i++;
+ }
 
-      return i;
-   }
+ return i;
+}
 
 
    /**
@@ -17,47 +19,49 @@ public class Markdown {
     * @return wrapped text (eg: <div id="conent">wrapped</div>)
     */
    public static String tagWrap(String toWrap, String tag) {
-      return "<" + tag + ">" + toWrap + "</" + tag.split(" ")[0] + ">";
-   }
+    return "<" + tag + ">" + toWrap + "</" + tag.split(" ")[0] + ">";
+  }
 
    /**
     * Will run the parser, line per line
+    * @return {String} HTML.
     */
    public static String Parse(String md) {
-      String lines[] = md.split("\n");
+    String lines[] = md.split("\n");
+    int oneUnderscore[] = null;
+    boolean oneStar = false;
 
-      String result = "";
+    String result = "";
 
-      for(String line : lines) {
-         int h = parseHeader(line);
-
-         
-
-         if (h > 0 && h<= 6) {
-            line = tagWrap(line.substring(h), "h" + h);
-         }
-
-         result += line +="\n";
+    for(int i = 0; i < lines.length; i++) {
+      String line = lines[i];
+      
+      // Find headers and repalce them.
+      int h = parseHeader(line);
+      if (h > 0 && h<= 6) {
+        line = tagWrap(line.substring(h), "h" + h);
       }
 
-      
-      return result;
-   }
+      result += line +="\n";
+    }
 
-   public static void test() {
-      String wrapMe = "Wrap this in tags?";
-      System.out.println(tagWrap(wrapMe, "p"));
-      System.out.println(tagWrap(wrapMe, "div"));
-      System.out.println(tagWrap(wrapMe, "a href=\"#awesome\""));
 
-      String parseMe = "#this is a H1 tag\nthis is a body\n##double h2!\nanother line.";
-      System.out.println(Parse(parseMe));
-   }
+    return result;
+  }
+
+  public static void test() { String wrapMe = "Wrap this in tags?";
+  System.out.println(tagWrap(wrapMe, "p"));
+  System.out.println(tagWrap(wrapMe, "div"));
+  System.out.println(tagWrap(wrapMe, "a href=\"#awesome\""));
+
+  String parseMe = "#this is a H1 tag\nthis is a body\n##double h2!\nanother line.";
+  System.out.println(Parse(parseMe));
+}
 
    /**
     *
     */
    public static void main(String[] args) {
-      test();
-   }
+    test();
+  }
 }
