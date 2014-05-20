fafe6be3b4d64a8589db69230193df2617bd312d
Gage <justgage@gmail.com>
2014-05-20 05:59:05 -0600

    Changed Markdown Interpeter over to a Singleton
    Also started on the character by character parsing

--- Markdown.java
+++ Markdown.java
@@ -1,17 +1,57 @@
 import java.util.regex.*;
-
+import java.lang.Character;
+/**
+* @singleton Class that parses markdown
+*/
 public class Markdown {
 
   /**
   * @return Header level (eg: 1) if no header it will be 0
   */
-  public static int parseHeader(String line) {
+
+  static Markdown singleton;
+
+  private Markdown() {
+    if (singleton != null) {
+      singleton = new Markdown();
+    }
+  }
+
+  public static Markdown getInstance() {
+    return singleton;
+  }
+
+  public int parseHeader(String line) {
     int i = 0;
     while (line.charAt(i) == '#') {
       i++;
     }
 
     return i;
+  }
+
+  public String parseChars(String allText) {
+    char cursor;
+    int lastEm = -1;
+    int lastStrong = -1;
+
+    for (int i = 0; i < allText.length(); i++) {
+      cursor = allText.charAt(i);
+      switch (cursor) {
+      case '_':
+        //checks if previous was whitespace
+        if (i - 1 < 0 || Character.isWhitespace(allText.charAt(i - 1))) {
+          if (allText.charAt(i + 1) == '_') {
+            if (lastStrong != -1) {
+
+            }
+          }
+
+        }
+      }
+    }
+
+    return allText;
   }
 
 
@@ -21,7 +61,7 @@ public class Markdown {
    *    tag - tag text "div id="content""
    * @return wrapped text (eg: <div id="conent">wrapped</div>)
    */
-  public static String tagWrap(String toWrap, String tag) {
+  public String tagWrap(String toWrap, String tag) {
     return "<" + tag + ">" + toWrap + "</" + tag.split(" ")[0] + ">";
   }
 
@@ -29,7 +69,7 @@ public class Markdown {
    * Will run the parser, line per line
    * @return {String} HTML.
    */
-  public static String Parse(String md) {
+  public String Parse(String md) {
     String lines[] = md.split("\n");
     int oneUnderscore[] = null;
     boolean oneStar = false;
@@ -52,7 +92,7 @@ public class Markdown {
     return result;
   }
 
-  public static void test() {
+  public void test() {
     String wrapMe = "Wrap this in tags?";
     System.out.println(tagWrap(wrapMe, "p"));
     System.out.println(tagWrap(wrapMe, "div"));
@@ -66,6 +106,6 @@ public class Markdown {
    *
    */
   public static void main(String[] args) {
-    test();
+    Markdown.getInstance().test();
   }
 }
